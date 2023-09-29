377511ef54bb642d3360ddeda276781757a3fe68
Rafael Velazquez <rafavel555@gmail.com>
2023-09-29 11:43:38 -0600

    Se agrego UsuariosAccesoDatos

---
+++ ProyectoAutos/ProyectoAutos/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace ProyectoAutos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Usuarios = new System.Windows.Forms.TextBox();
+            this.txt_Contraseña = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Ingresar = new System.Windows.Forms.Button();
+            this.btn_Salir = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_Contraseña);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txt_Usuarios);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Bookman Old Style", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 198);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(347, 174);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_Salir);
+            this.groupBox2.Controls.Add(this.btn_Ingresar);
+            this.groupBox2.Location = new System.Drawing.Point(12, 378);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(347, 60);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario";
+            // 
+            // txt_Usuarios
+            // 
+            this.txt_Usuarios.Location = new System.Drawing.Point(59, 58);
+            this.txt_Usuarios.Name = "txt_Usuarios";
+            this.txt_Usuarios.Size = new System.Drawing.Size(234, 23);
+            this.txt_Usuarios.TabIndex = 1;
+            // 
+            // txt_Contraseña
+            // 
+            this.txt_Contraseña.Location = new System.Drawing.Point(59, 119);
+            this.txt_Contraseña.Name = "txt_Contraseña";
+            this.txt_Contraseña.Size = new System.Drawing.Size(234, 23);
+            this.txt_Contraseña.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Contraseña";
+            // 
+            // btn_Ingresar
+            // 
+            this.btn_Ingresar.Location = new System.Drawing.Point(253, 19);
+            this.btn_Ingresar.Name = "btn_Ingresar";
+            this.btn_Ingresar.Size = new System.Drawing.Size(88, 35);
+            this.btn_Ingresar.TabIndex = 0;
+            this.btn_Ingresar.Text = "Ingresar";
+            this.btn_Ingresar.UseVisualStyleBackColor = true;
+            // 
+            // btn_Salir
+            // 
+            this.btn_Salir.Location = new System.Drawing.Point(6, 19);
+            this.btn_Salir.Name = "btn_Salir";
+            this.btn_Salir.Size = new System.Drawing.Size(91, 35);
+            this.btn_Salir.TabIndex = 1;
+            this.btn_Salir.Text = "Salir";
+            this.btn_Salir.UseVisualStyleBackColor = true;
+            this.btn_Salir.Click += new System.EventHandler(this.btn_Salir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txt_Contraseña;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_Usuarios;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Salir;
+        private System.Windows.Forms.Button btn_Ingresar;
+    }
+}
+
---
+++ ProyectoAutos/UsuariosAccesoDatos/Conexion.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace UsuariosAccesoDatos
+{
+    public class Conexion
+    {
+        private MySqlConnection _conex;
+
+        public Conexion(string servidor, string usuario, string password, string database, uint puerto)
+        {
+            MySqlConnectionStringBuilder cadenaConexion = new MySqlConnectionStringBuilder();
+            cadenaConexion.Server = servidor;
+            cadenaConexion.UserID = usuario;
+            cadenaConexion.Password = password;
+            cadenaConexion.Database = database;
+            cadenaConexion.Port = puerto;
+
+            _conex = new MySqlConnection(cadenaConexion.ToString());
+        }
+        public void EjecutarConsulta(string consulta)
+        {
+            try
+            {
+                _conex.Open();
+                using (MySqlCommand comando = new MySqlCommand(consulta, _conex))
+                {
+                    comando.ExecuteNonQuery();
+                    Console.WriteLine("Consulta ejecutada correctamente");
+                }
+                _conex.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al ejecutar la consulta: ", ex.Message);
+            }
+        }
+        //DataTable funciona con una tabla, DataSet varias tablas señalando su posición
+        public DataTable ObtenerDatos(string consulta)
+        {
+            DataTable tabla = new DataTable();
+            try
+            {
+                _conex.Open();
+                using (MySqlCommand comando = new MySqlCommand(consulta, _conex))
+                {
+                    using (MySqlDataAdapter adaptador = new MySqlDataAdapter(comando))
+                    {
+                        adaptador.Fill(tabla);
+                        Console.WriteLine("Consulta ejecutada correctamente");
+                    }
+                }
+                _conex.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al ejecutar la consulta: ", ex.Message);
+            }
+            return tabla;
+        }
+    }
+
+}
