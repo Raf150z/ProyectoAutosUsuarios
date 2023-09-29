d61afd0f5be71075d65fe3fcda2ad2a1409defd6
Rafael Velazquez <rafavel555@gmail.com>
2023-09-29 12:34:16 -0600

    Se agregaron las conexiones a la BD

--- ProyectoAutos/UsuariosAccesoDatos/AccesoDatos.cs
+++ ProyectoAutos/UsuariosAccesoDatos/AccesoDatos.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UsuariosAccesoDatos;
+using UsuariosEntidades;
+using System.Data;
+using MySql.Data.MySqlClient;
 
 namespace UsuariosAccesoDatos
 {
+    
     public class AccesoDatos
     {
+        Conexion Con;
 
+        public AccesoDatos()
+        {
+            Con = new Conexion("localhost", "root", "", "Users", 3306);
+        }
+        public int Login(string Usuario, String Contraseña)
+        {
+            Con._conex.Open();
+            string Consulta = string.Format("select count(*) from Usuarios where nusuario = '{0}' and pass = '{1}'", Usuario, Contraseña);
+            MySqlCommand Com = new MySqlCommand(Consulta, Con._conex);
+            int Sesion = Convert.ToInt32(Com.ExecuteScalar());
+            Con._conex.Close();
+            return Sesion;
+        }
     }
 }
---
+++ ProyectoAutos/UsuariosEntidades/Usuarios.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsuariosEntidades
+{
+    public class Usuarios
+    {
+        private int _idusuario;
+        private string _nusuario;
+        private string _pass;
+
+
+        public int IdUsuario { get => _idusuario; set => _idusuario = value; }
+        public string Nusuario { get => _nusuario;set => _nusuario = value; }
+        public string Pass { get => _pass; set => _pass = value; }
+
+    }
+}
